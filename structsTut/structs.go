93c3cc54095052b4cf2b4e63166612cb6cf8ebb0
Your Name <itismepranish@gmail.com>
2026-06-05 19:04:08 +0545

    Pointers in structs

--- structsTut/structs.go
+++ structsTut/structs.go
@@ -1,26 +1,24 @@
 package structsTut
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Student struct {
 	name    string
 	age     uint8
 	address string
 	email   string
-	course  Course
+	Course
 }
 
 type Course struct {
-	name         string
+	CourseName   string
 	duration     uint8
 	moduleLeader string
 }
 
 func Computing() {
 	BSC_Computing := Course{
-		name:         "BSc Hons in Computing",
+		CourseName:   "BSc Hons in Computing",
 		duration:     3,
 		moduleLeader: "Anushree Paudyal",
 	}
@@ -29,7 +27,38 @@ func Computing() {
 		age:     20,
 		address: "Kadaghari",
 		email:   "pranish@gmail.com",
-		course:  BSC_Computing,
+		Course:  BSC_Computing,
 	}
-	fmt.Println(Pranish.name, Pranish.age, Pranish.address, Pranish.email, Pranish.course.name, Pranish.course.duration, Pranish.course.moduleLeader)
+
+	var moduleLeader, moduleLeaderErr = getModuleLeaderOfStudent(&Pranish)
+	if moduleLeaderErr != nil {
+		fmt.Println(moduleLeaderErr)
+	} else {
+		fmt.Printf("Module Leader of %s is %s\n", Pranish.name, moduleLeader)
+	}
+
+	var isAbove18, ageErr = isStudentAbove18(&Pranish)
+	if ageErr != nil {
+		fmt.Println(ageErr)
+	} else if isAbove18 {
+		fmt.Printf("%s is above 18\n", Pranish.name)
+	} else {
+		fmt.Printf("%s is below 18\n", Pranish.name)
+	}
+
+}
+
+func getModuleLeaderOfStudent(student *Student) (string, error) {
+
+	if student == nil || student.moduleLeader == "" {
+		return "", fmt.Errorf("module leader not found")
+	}
+	return student.moduleLeader, nil
+}
+
+func isStudentAbove18(student *Student) (bool, error) {
+	if student == nil {
+		return false, fmt.Errorf("student not found")
+	}
+	return student.age > 18, nil
 }
