e3f921e168df05e36d6526662fb20bcbefffbc71
Your Name <itismepranish@gmail.com>
2026-06-05 18:50:14 +0545

    structs

--- structsTut/structs.go
+++ structsTut/structs.go
@@ -1,22 +1,35 @@
 package structsTut
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Student struct {
 	name    string
 	age     uint8
 	address string
 	email   string
-	course  string
+	course  Course
+}
+
+type Course struct {
+	name         string
+	duration     uint8
+	moduleLeader string
 }
 
 func Computing() {
+	BSC_Computing := Course{
+		name:         "BSc Hons in Computing",
+		duration:     3,
+		moduleLeader: "Anushree Paudyal",
+	}
 	Pranish := Student{
 		name:    "Pranish",
 		age:     20,
 		address: "Kadaghari",
 		email:   "pranish@gmail.com",
-		course:  "Go",
+		course:  BSC_Computing,
 	}
-	fmt.Println(Pranish.name, Pranish.age, Pranish.address, Pranish.email, Pranish.course)
+	fmt.Println(Pranish.name, Pranish.age, Pranish.address, Pranish.email, Pranish.course.name, Pranish.course.duration, Pranish.course.moduleLeader)
 }
